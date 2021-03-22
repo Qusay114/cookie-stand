3b2e40133eb6764a4e1a9a779a10fde06bfa0bb9
Qusay114 <eng.qusay015@gmail.com>
2021-03-22 20:04:30 +0200

    convert the objects to one object then used constructor to create instances

--- js/app.js
+++ js/app.js
@@ -1,261 +1,176 @@
 'use strict';
 
+//to generate a random function
 function randomNumber(min,max)
 {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-const seattleData  =
+//to create a table element with dynamic number of cells and headers , note: the container should be passed as a id for the element
+function createTable(container,id,rowNum , coloumnNum , headerRow)
 {
-  minCust:23,
-  maxCust:65,
-  averageCperC:6.3,
-  startHour:6,
-  endHour:19,
-  salesPH : [],
-  totalSales:0,
-  fillData : function()
+  let rowElements = []; //this array to hold the row elements
+  let rowColElements = []; //this array to hold the coloumn elements
+  const tableEl = document.createElement('table');
+  tableEl.id = id;
+  const Container = document.getElementById(container);
+  Container.appendChild(tableEl);
+  for(let r = 0 ; r < rowNum ; r++ )
   {
-    for(let i = 0; i <= this.endHour-this.startHour ; i++)
+    rowElements.push(document.createElement('tr'));
+    tableEl.appendChild(rowElements[r]);
+    rowColElements.push([]); //here I push empty array so I can fill data as 2-D array
+    for(let c = 0 ; c < coloumnNum ; c++)
     {
-      console.log(randomNumber(this.minCust,this.maxCust));
-      this.salesPH.push(Math.floor(randomNumber(this.minCust,this.maxCust)*this.averageCperC));
-    }
-  },
-  showData : function()
-  {
-    const container = document.getElementById('container');
-    const locationName = document.createElement('h3');
-    container.appendChild(locationName);
-    locationName.textContent = 'Seatle daily data :';
-    const ulElement = document.createElement('ul');
-    container.appendChild(ulElement);
-    let counter = this.startHour;
-    for(let i = 0; i <this.salesPH.length; i++)
-    {
-      this.totalSales = this.totalSales + this.salesPH[i];
-      const listItem = document.createElement('li');
-      ulElement.appendChild(listItem);
-      if(counter<12)
-        listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
-      else if(counter===12)
-        listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
+      if(r===0 && headerRow === true) //here I check if it wants a header in the first row or not
+      {
+        rowColElements[r].push(document.createElement('th'));
+        rowElements[r].appendChild(rowColElements[r][c]);
+      }
       else
-        listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
-      counter ++ ;
+      {
+        rowColElements[r].push(document.createElement('td'));
+        rowElements[r].appendChild(rowColElements[r][c]);
+      }
     }
+  }
+  return rowColElements;
+}
+
+
+//to create an object for daily data store where every hour has its number of purchases for each loaction (seattle , tokyo , ...)
+function DailyData(location,minCustPH , maxCustPH , averageCperC , startHour , endHour)
+{
+  this.location = location;
+  this.minCustPH = minCustPH;
+  this.maxCustPH = maxCustPH;
+  this.averageCperC = averageCperC;
+  this.startHour = startHour;
+  this.endHour = endHour;
+  this.salesPH = [];
+  this.custPH = [];
+  this.totalSales = 0;
+}
+
+//to create a method for DailyData object that will fill the data of purchases for each hour and sum the total
+DailyData.prototype.fillData = function()
+{
+  for(let i = 0; i <= this.endHour-this.startHour ; i++)
+  {
+    this.custPH.push((randomNumber(this.minCustPH,this.maxCustPH)) );
+    this.salesPH.push(Math.floor(this.custPH[i]*this.averageCperC));
+    this.totalSales = this.totalSales + this.salesPH[i];
+  }
+};
+
+//to create a method to show the data on the page where will create html element and put the data we got from fillData method
+DailyData.prototype.showData = function ()
+{
+  const container = document.getElementById('container');
+  const locationName = document.createElement('h3');
+  container.appendChild(locationName);
+  locationName.textContent = `${this.location} daily data :`;
+  const ulElement = document.createElement('ul');
+  container.appendChild(ulElement);
+  let counter = this.startHour;
+  for(let i = 0; i <this.salesPH.length; i++)
+  {
     const listItem = document.createElement('li');
     ulElement.appendChild(listItem);
-    listItem.textContent = `Total: ${this.totalSales} cookies`;
+    if(counter<12)
+      listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
+    else if(counter===12)
+      listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
+    else
+      listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
+    counter ++ ;
   }
-
+  const listItem = document.createElement('li');
+  ulElement.appendChild(listItem);
+  listItem.textContent = `Total: ${this.totalSales} cookies`;
 };
 
 
-seattleData.fillData();
-seattleData.showData();
+//to create instances and give for each of them arguemnts fit with the location , then use the fill data and show data methods to fill and show the data on the page
+const Seattle = new DailyData('Seattle',23,65,6.3,6,19);
+Seattle.fillData();
+
+const Tokyo = new DailyData('Tokyo',3,24,1.2,6,19);
+Tokyo.fillData();
+
+const Dubai = new DailyData('Dubai',11,38,3.7,6,19);
+Dubai.fillData();
+
+const Paris = new DailyData('Paris',20,38,2.3,6,19);
+Paris.fillData();
+
+const Lima = new DailyData('Lima',2,16,4.6,6,19);
+Lima.fillData();
 
 
-const tokyoData  =
+//to create a table then fill it with data of the instances
+const cells = createTable('container','dailyDataTable',7,16,true); //here is the syntax : createTable(container,id,rows,coloumns,Do you want a header row ?)
+let counter = Seattle.startHour - 1;//this -1 because I left an empty cell before start putting the hours
+let endH = Seattle.endHour;
+let sumdailyTotal = 0;
+let sumHourlyTotal = 0;
+cells[0][0].textContent='';
+for(let c = 0 ; c<16; c++)
 {
-  minCust:3,
-  maxCust:24,
-  averageCperC:1.2,
-  startHour:6,
-  endHour:19,
-  salesPH : [],
-  totalSales:0,
-  fillData : function()
+  //to fill the row of hours
+  if(counter<12 && c !== 0)
   {
-    for(let i = 0; i <= this.endHour-this.startHour ; i++)
-    {
-      console.log(randomNumber(this.minCust,this.maxCust));
-      this.salesPH.push(Math.floor(randomNumber(this.minCust,this.maxCust)*this.averageCperC));
-    }
-  },
-  showData : function()
+    cells[0][c].textContent = `${counter} am`;
+  }
+  else if(counter === 12 && c !== 0)
   {
-    const container = document.getElementById('container');
-    const locationName = document.createElement('h3');
-    container.appendChild(locationName);
-    locationName.textContent = 'Tokyo daily data :';
-    const ulElement = document.createElement('ul');
-    container.appendChild(ulElement);
-    let counter = this.startHour;
-    for(let i = 0; i <this.salesPH.length; i++)
-    {
-      this.totalSales = this.totalSales + this.salesPH[i];
-      const listItem = document.createElement('li');
-      ulElement.appendChild(listItem);
-      if(counter<12)
-        listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
-      else if(counter===12)
-        listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
-      else
-        listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
-      counter ++ ;
-    }
-    const listItem = document.createElement('li');
-    ulElement.appendChild(listItem);
-    listItem.textContent = `Total: ${this.totalSales} cookies`;
+    cells[0][c].textContent = `${counter} pm`;
+  }
+  else if(counter <= endH && c !== 0)
+  {
+    cells[0][c].textContent = `${counter - 12} pm`;
+  }
+  counter ++ ;
+
+  //to fill the data of each location with its purchase cookies per hour
+  if(c<14)
+  {
+
+    cells[1][c+1].textContent = Seattle.salesPH[c];
+
+    cells[2][c+1].textContent = Tokyo.salesPH[c];
+
+    cells[3][c+1].textContent = Dubai.salesPH[c];
+
+    cells[4][c+1].textContent = Paris.salesPH[c];
+
+    cells[5][c+1].textContent = Lima.salesPH[c];
+
+    cells[6][0].textContent = 'Total';
+    sumHourlyTotal = Seattle.salesPH[c] + Tokyo.salesPH[c] + Dubai.salesPH[c] + Paris.salesPH[c] + Lima.salesPH[c] ;
+    cells[6][c+1].textContent = sumHourlyTotal ;
+    sumdailyTotal = sumdailyTotal + sumHourlyTotal;
   }
 
-};
+}
 
 
-tokyoData.fillData();
-tokyoData.showData();
+//to fill the location name and the total sales of the day for each location
+cells[0][15].textContent = 'Daily Location Total';
+cells[1][0].textContent = Seattle.location;
+cells[1][15].textContent = Seattle.totalSales;
 
+cells[2][0].textContent = Tokyo.location;
+cells[2][15].textContent = Tokyo.totalSales;
 
-const dubaiData  =
-{
-  minCust:11,
-  maxCust:38,
-  averageCperC:3.7,
-  startHour:6,
-  endHour:19,
-  salesPH : [],
-  totalSales:0,
-  fillData : function()
-  {
-    for(let i = 0; i <= this.endHour-this.startHour ; i++)
-    {
-      console.log(randomNumber(this.minCust,this.maxCust));
-      this.salesPH.push(Math.floor(randomNumber(this.minCust,this.maxCust)*this.averageCperC));
-    }
-  },
-  showData : function()
-  {
-    const container = document.getElementById('container');
-    const locationName = document.createElement('h3');
-    container.appendChild(locationName);
-    locationName.textContent = 'Dubai daily data :';
-    const ulElement = document.createElement('ul');
-    container.appendChild(ulElement);
-    let counter = this.startHour;
-    for(let i = 0; i <this.salesPH.length; i++)
-    {
-      this.totalSales = this.totalSales + this.salesPH[i];
-      const listItem = document.createElement('li');
-      ulElement.appendChild(listItem);
-      if(counter<12)
-        listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
-      else if(counter===12)
-        listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
-      else
-        listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
-      counter ++ ;
-    }
-    const listItem = document.createElement('li');
-    ulElement.appendChild(listItem);
-    listItem.textContent = `Total: ${this.totalSales} cookies`;
-  }
+cells[3][0].textContent = Dubai.location;
+cells[3][15].textContent = Dubai.totalSales;
 
-};
+cells[4][0].textContent = Paris.location;
+cells[4][15].textContent = Paris.totalSales;
 
+cells[5][0].textContent = Lima.location;
+cells[5][15].textContent = Lima.totalSales;
 
-dubaiData.fillData();
-dubaiData.showData();
+cells[6][15].textContent = sumdailyTotal; //the total daily of the total hourly for all locations
 
-
-const parisData  =
-{
-  minCust:20,
-  maxCust:38,
-  averageCperC:2.3,
-  startHour:6,
-  endHour:19,
-  salesPH : [],
-  totalSales:0,
-  fillData : function()
-  {
-    for(let i = 0; i <= this.endHour-this.startHour ; i++)
-    {
-      console.log(randomNumber(this.minCust,this.maxCust));
-      this.salesPH.push(Math.floor(randomNumber(this.minCust,this.maxCust)*this.averageCperC));
-    }
-  },
-  showData : function()
-  {
-    const container = document.getElementById('container');
-    const locationName = document.createElement('h3');
-    container.appendChild(locationName);
-    locationName.textContent = 'Paris daily data :';
-    const ulElement = document.createElement('ul');
-    container.appendChild(ulElement);
-    let counter = this.startHour;
-    for(let i = 0; i <this.salesPH.length; i++)
-    {
-      this.totalSales = this.totalSales + this.salesPH[i];
-      const listItem = document.createElement('li');
-      ulElement.appendChild(listItem);
-      if(counter<12)
-        listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
-      else if(counter===12)
-        listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
-      else
-        listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
-      counter ++ ;
-    }
-    const listItem = document.createElement('li');
-    ulElement.appendChild(listItem);
-    listItem.textContent = `Total: ${this.totalSales} cookies`;
-  }
-
-};
-
-
-parisData.fillData();
-parisData.showData();
-
-
-const limaData  =
-{
-  minCust:2,
-  maxCust:16,
-  averageCperC:4.6,
-  startHour:6,
-  endHour:19,
-  salesPH : [],
-  totalSales:0,
-  fillData : function()
-  {
-    for(let i = 0; i <= this.endHour-this.startHour ; i++)
-    {
-      console.log(randomNumber(this.minCust,this.maxCust));
-      this.salesPH.push(Math.floor(randomNumber(this.minCust,this.maxCust)*this.averageCperC));
-    }
-  },
-  showData : function()
-  {
-    const container = document.getElementById('container');
-    const locationName = document.createElement('h3');
-    container.appendChild(locationName);
-    locationName.textContent = 'Lima daily data :';
-    const ulElement = document.createElement('ul');
-    container.appendChild(ulElement);
-    let counter = this.startHour;
-    for(let i = 0; i <this.salesPH.length; i++)
-    {
-      this.totalSales = this.totalSales + this.salesPH[i];
-      const listItem = document.createElement('li');
-      ulElement.appendChild(listItem);
-      if(counter<12)
-        listItem.textContent = `${counter}am: ${this.salesPH[i]} cookies`;
-      else if(counter===12)
-        listItem.textContent = `${counter}pm: ${this.salesPH[i]} cookies`;
-      else
-        listItem.textContent = `${counter-12}pm: ${this.salesPH[i]} cookies`;
-      counter ++ ;
-    }
-    const listItem = document.createElement('li');
-    ulElement.appendChild(listItem);
-    listItem.textContent = `Total: ${this.totalSales} cookies`;
-  }
-
-};
-
-
-limaData.fillData();
-limaData.showData();
